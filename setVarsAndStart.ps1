6d37eac20acbed8b1a05f2540ac38830345d0c14
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-07-28 17:38:56 +0300

    Upload setVarsAndStart.ps1

---
+++ setVarsAndStart.ps1
@@ -0,0 +1,25 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+
+#ftp settings
+$ftpLogin = ""
+$ftpPassword = ""
+$ftpAddress = "ftp.soft-fx.lv"
+$ftpLocalFolder = ($MyInvocation.MyCommand.Definition | split-path -parent)+"\Dumps"
+$ftpRemoteFolder = "/TickTrader/AdminTools/dumps"
+
+
+#list of processes and thresholdes
+$processes = @("ordercleaner.vshost.exe","devenv.exe")
+$threshold = 20000000 #in bytes
+
+#for email sending
+$emailsToSend = "yahor.mazko@soft-fx.lv"
+$login = "staging@st.soft-fx.eu"
+$password = "detre@Re5h"
+$SMTPServer = "mx.st.soft-fx.eu"
+$SMTPPort = "25"
+
+$scriptFolder = $MyInvocation.MyCommand.Definition | split-path -parent
+cd $scriptFolder
+
+& .\DumpMonitor.ps1
